adc6b503d4125b2bfa23b7c2b098bb534941b15a
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-04-12 01:54:27 +0600

    email password authentication,requireauth added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./App.css";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
 import Inventory from "./components/Inventory/Inventory";
+import LogIn from "./components/LogIn/LogIn";
 import NotFound from "./components/NotFound/NotFound";
 import Order from "./components/Order/Order";
+import RequireAuth from "./components/RequireAuth/RequireAuth";
 import Shop from "./components/Shop/Shop";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
@@ -14,9 +17,18 @@ function App() {
       <Routes>
         <Route path="/" element={<Shop></Shop>}></Route>
         <Route path="/shop" element={<Shop></Shop>}></Route>
-        <Route path="/order" element={<Order></Order>}></Route>
+        <Route
+          path="/order"
+          element={
+            <RequireAuth>
+              <Order></Order>
+            </RequireAuth>
+          }
+        ></Route>
         <Route path="/inventory" element={<Inventory></Inventory>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<LogIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
